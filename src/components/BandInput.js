b0a318cc6c5499f61046d5d95603a5fc7a8b7b11
cesmiley <catherinesmiley@gmail.com>
2020-09-23 23:38:21 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,77 @@
-// Add BandInput component
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+
+// class BandInput extends Component {
+
+//   state = {
+//     name: ''
+//   }
+
+//   handleChange = e => {
+//     this.setState({ name: e.target.value })
+//   }
+
+//   handleSubmit = e => {
+//     e.preventDefault()
+//     this.props.addBand(this.state)
+//     this.setState({
+//       name: ''
+//     })
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         <form onSubmit={e => this.handleSubmit(e)}>
+//           <input 
+//             type="text"
+//             onChange={e => this.handleChange(e)}
+//             name="name"
+//           />
+//           <input type="submit"></input>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+
+// export default BandInput
+
+  
+import React, { Component } from 'react';
 
 class BandInput extends Component {
+  state = {
+    name: '',
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: '',
+    });
+  }
+
   render() {
-    return(
+    return (
       <div>
-        Band Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type='text'
+            value={this.state.name}
+            onChange={(event) => this.handleOnChange(event)}
+          />
+          <input type='submit' />
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default BandInput
+export default BandInput;
